c6673a06ee76614fe1e378a17b1023839280b9f1
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2021-09-12 16:23:14 -0400

    dev nice things

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use framebuffer::{Framebuffer, KdMode};
 //use nix::ioctl_write_buf;
 
 use std::os::unix::io::AsRawFd;
+use std::process::Command;
 
 use evdev::RelativeAxisType;
 
@@ -19,13 +20,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
   
   let mut state = State {
     fb: Framebuffer::new(&fb_path)?,
+    frame: None,
     tick: 0,
     cursors: vec![],
     exit: false,
   };
-  let mut remaining_errors: usize = 6000;
+  let mut remaining_errors: usize = 900;
   
-  Framebuffer::set_kd_mode(KdMode::Graphics)?;
+  let tty_to_return_to_ifn_in_tty = std::env::var("JSKEU_TTY_RETURN_NUM").unwrap_or("1".to_string()); // I always use tty1 as my x11 GUI
+  let tty_to_go_to_ifn_in_tty = std::env::var("JSKEU_TTY_GOTO_NUM").unwrap_or("2".to_string()); // generally unused
+  let we_were_in_tty = punwrap_r!(Framebuffer::set_kd_mode(KdMode::Graphics), tf);
+  if (!we_were_in_tty) {
+    eprintln!("Attempting to use chvt to go to tty {}", &tty_to_go_to_ifn_in_tty);
+    punwrap_r!(Command::new("sudo")
+      .args(&["chvt", &tty_to_go_to_ifn_in_tty])
+      .status(), nop);
+  }
 
   loop {
 
@@ -54,13 +64,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
   }
 
-  Framebuffer::set_kd_mode(KdMode::Text)?;
+  if we_were_in_tty {
+    // go back to cli mode
+    punwrap_r!(Framebuffer::set_kd_mode(KdMode::Text), nop);
+  }
+  else {
+    // switch to the x11 gui
+    eprintln!("Attempting to use chvt to return to tty {}", &tty_to_return_to_ifn_in_tty);
+    punwrap_r!(Command::new("sudo")
+      .args(&["chvt", &tty_to_return_to_ifn_in_tty])
+      .status(), nop);
+  }
+  
   Ok(())
 }
 
 
 struct State {
   pub fb: Framebuffer,
+  pub frame: Option<Vec<u8>>,
   pub tick: u64,
   pub cursors: Vec<Cursor>,
   //pub keyboards: Vec<Keyboard>,
@@ -115,8 +137,10 @@ fn state_update(state: &mut State) -> Result<(), Box<dyn std::error::Error>> {
     }
   }
 
-  // For each known mouse read position
-
+  // Every tick, for each known mouse read position
+  for known in &mut state.cursors {
+    known.x = 5;
+  }
 
 
   Ok(())
@@ -129,12 +153,111 @@ fn frame(state: &mut State) -> Result<(), Box<dyn std::error::Error>> {
   let line_length = state.fb.fix_screen_info.line_length;
   let bytespp = state.fb.var_screen_info.bits_per_pixel / 8;
 
-  let mut frame = vec![0u8; (line_length * h) as usize];
+  if ! state.frame.is_some() {
+    state.frame = Some(vec![0u8; (line_length * h) as usize]);
+  }
+
+  if let Some(ref mut frame) = &mut state.frame {
+    
+    // First draw BG (TODO not this, srsly bad performance)
+    for (r, line) in frame.chunks_mut(line_length as usize).enumerate() {
+      for (c, p) in line.chunks_mut(bytespp as usize).enumerate() {
+        write_pixel_rgb!(p, 12, 12, 12);
+      }
+    }
+
+    // 2nd draw mice
+    for known in &state.cursors {
+      // get i from x+y coords and draw
+      
 
 
+    }
+    
+    
+    // Finally write + activate the frame
+    state.fb.write_frame(&frame);
+    
+    unsafe {
+      activate_fb(state.fb.device.as_raw_fd());
+    }
+  }
 
   Ok(())
 }
+
+/* Utilities */
+
+/*
+ * Assuming r+g+b buffer in p, write the rgb values in at offsets 0,1,2
+ */
+#[macro_export]
+macro_rules! write_pixel_rgb {
+  ($p:expr, $r:expr, $g:expr, $b:expr) => {
+    $p[0] = $r;
+    $p[1] = $g;
+    $p[2] = $b;
+  }
+}
+
+
+/*
+ * The "Print Unwrap" macro simply performs .unwrap(),
+ * but prints errors to stdout and performs some control
+ * flow (return, continue, break) instead of panicing.
+ * This was copied directly from the v3 core library where plugins
+ * made more re-use of the macro, here it is only used in permission checks
+ * that are guaranteed to fail on win64 with a FAT32 filesystem.
+ */
+#[macro_export]
+macro_rules! punwrap_r {
+    ($e:expr, continue) => {
+      match $e {
+        Ok(val) => val,
+        Err(e) => {
+          eprintln!("{}:{} e={:#?}", file!(), line!(), e);
+          continue;
+        }
+      }
+    };
+    ($e:expr, break) => {
+      match $e {
+        Ok(val) => val,
+        Err(e) => {
+          eprintln!("{}:{} e={:#?}", file!(), line!(), e);
+          break;
+        }
+      }
+    };
+    ($e:expr, return) => {
+      match $e {
+        Ok(val) => val,
+        Err(e) => {
+          eprintln!("{}:{} e={:#?}", file!(), line!(), e);
+          return;
+        }
+      }
+    };
+    ($e:expr, nop) => {
+      match $e {
+        Ok(val) => (),
+        Err(e) => {
+          eprintln!("{}:{} e={:#?}", file!(), line!(), e);
+        }
+      }
+    };
+    ($e:expr, tf) => { // yields true of expression .is_some()/.is_ok(), else false
+      match $e {
+        Ok(val) => true,
+        Err(e) => {
+          eprintln!("{}:{} e={:#?}", file!(), line!(), e);
+          false
+        }
+      }
+    };
+}
+
+
 
 
 /* old code:
