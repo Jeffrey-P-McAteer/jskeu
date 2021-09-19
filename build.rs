8e806b767e4109629063850c0876b77d695dff1f
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2021-09-19 15:53:31 -0400

    stole a ton of OpenGL example code for fullscreen ops

--- build.rs
+++ build.rs
@@ -22,8 +22,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
   }
 
+  generate_gl_bindings();
 
   Ok(())
 }
 
+fn generate_gl_bindings() {
+  use gl_generator::{Api, Fallbacks, Profile, Registry};
+  use std::path::PathBuf;
+  use std::fs::File;
+
+  let dest = PathBuf::from(&env::var("OUT_DIR").unwrap());
+  let mut file = File::create(&dest.join("gl_bindings.rs")).unwrap();
+    Registry::new(Api::Gles2, (3, 3), Profile::Core, Fallbacks::All, [])
+        .write_bindings(gl_generator::StructGenerator, &mut file)
+        .unwrap();
+}
 
--- src/jskeu.rs
+++ src/jskeu.rs
@@ -2,7 +2,479 @@
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   
-  
+  exp01()?;
+
   Ok(())
 }
+
+
+use glutin::event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent};
+use glutin::event_loop::{ControlFlow, EventLoop};
+use glutin::monitor::{MonitorHandle, VideoMode};
+use glutin::window::{Fullscreen, WindowBuilder};
+use std::io::{stdin, stdout, Write};
+
+fn exp01() -> Result<(), Box<dyn std::error::Error>> {
+  let el = EventLoop::new();
+
+  print!("Please choose the fullscreen mode: (1) exclusive, (2) borderless: ");
+  stdout().flush()?;
+
+  let mut num = String::new();
+  stdin().read_line(&mut num)?;
+  let num = num.trim().parse().ok().expect("Please enter a number");
+
+  let fullscreen = Some(match num {
+      1 => Fullscreen::Exclusive(prompt_for_video_mode(&prompt_for_monitor(&el))),
+      2 => Fullscreen::Borderless(Some(prompt_for_monitor(&el))),
+      _ => panic!("Please enter a valid number"),
+  });
+
+  println!("Press (F) to toggle fullscreen, (D) to toggle window decorations, and (M) to toggle maximized/minimized.");
+
+  let mut is_maximized = false;
+  let mut decorations = true;
+
+  let wb = WindowBuilder::new().with_title("Hello world!").with_fullscreen(fullscreen.clone());
+  let windowed_context = glutin::ContextBuilder::new().build_windowed(wb, &el)?;
+
+  let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+
+  let gl = load(&windowed_context.context());
+
+  el.run(move |event, _, control_flow| {
+      *control_flow = ControlFlow::Wait;
+
+      match event {
+          Event::WindowEvent { event, .. } => match event {
+              WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+              WindowEvent::Resized(physical_size) => {
+                  windowed_context.resize(physical_size);
+              }
+              WindowEvent::KeyboardInput {
+                  input: KeyboardInput { virtual_keycode: Some(virtual_code), state, .. },
+                  ..
+              } => match (virtual_code, state) {
+                  (VirtualKeyCode::Escape, _) => *control_flow = ControlFlow::Exit,
+                  (VirtualKeyCode::F, ElementState::Pressed) => {
+                      if windowed_context.window().fullscreen().is_some() {
+                          windowed_context.window().set_fullscreen(None);
+                      } else {
+                          windowed_context.window().set_fullscreen(fullscreen.clone());
+                      }
+                  }
+                  (VirtualKeyCode::S, ElementState::Pressed) => {
+                      println!("window.fullscreen {:?}", windowed_context.window().fullscreen());
+                  }
+                  (VirtualKeyCode::M, ElementState::Pressed) => {
+                      is_maximized = !is_maximized;
+                      windowed_context.window().set_maximized(is_maximized);
+                  }
+                  (VirtualKeyCode::D, ElementState::Pressed) => {
+                      decorations = !decorations;
+                      windowed_context.window().set_decorations(decorations);
+                  }
+                  _ => (),
+              },
+              _ => (),
+          },
+          Event::RedrawRequested(_) => {
+              gl.draw_frame([1.0, 0.5, 0.7, 1.0]);
+              windowed_context.swap_buffers().unwrap();
+          }
+          _ => {}
+      }
+  });
+
+  Ok(())
+}
+
+// Enumerate monitors and prompt user to choose one
+fn prompt_for_monitor(el: &EventLoop<()>) -> MonitorHandle {
+    for (num, monitor) in el.available_monitors().enumerate() {
+        println!("Monitor #{}: {:?}", num, monitor.name());
+    }
+
+    print!("Please write the number of the monitor to use: ");
+    stdout().flush().unwrap();
+
+    let mut num = String::new();
+    stdin().read_line(&mut num).unwrap();
+    let num = num.trim().parse().ok().expect("Please enter a number");
+    let monitor = el.available_monitors().nth(num).expect("Please enter a valid ID");
+
+    println!("Using {:?}", monitor.name());
+
+    monitor
+}
+
+fn prompt_for_video_mode(monitor: &MonitorHandle) -> VideoMode {
+    for (i, video_mode) in monitor.video_modes().enumerate() {
+        println!("Video mode #{}: {}", i, video_mode);
+    }
+
+    print!("Please write the number of the video mode to use: ");
+    stdout().flush().unwrap();
+
+    let mut num = String::new();
+    stdin().read_line(&mut num).unwrap();
+    let num = num.trim().parse().ok().expect("Please enter a number");
+    let video_mode = monitor.video_modes().nth(num).expect("Please enter a valid ID");
+
+    println!("Using {}", video_mode);
+
+    video_mode
+}
+
+use glutin::{self, PossiblyCurrent};
+
+use std::ffi::CStr;
+
+pub mod gl {
+    pub use self::Gles2 as Gl;
+    include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
+}
+
+pub struct Gl {
+    pub gl: gl::Gl,
+}
+
+pub fn load(gl_context: &glutin::Context<PossiblyCurrent>) -> Gl {
+    let gl = gl::Gl::load_with(|ptr| gl_context.get_proc_address(ptr) as *const _);
+
+    let version = unsafe {
+        let data = CStr::from_ptr(gl.GetString(gl::VERSION) as *const _).to_bytes().to_vec();
+        String::from_utf8(data).unwrap()
+    };
+
+    println!("OpenGL version {}", version);
+
+    unsafe {
+        let vs = gl.CreateShader(gl::VERTEX_SHADER);
+        gl.ShaderSource(vs, 1, [VS_SRC.as_ptr() as *const _].as_ptr(), std::ptr::null());
+        gl.CompileShader(vs);
+
+        let fs = gl.CreateShader(gl::FRAGMENT_SHADER);
+        gl.ShaderSource(fs, 1, [FS_SRC.as_ptr() as *const _].as_ptr(), std::ptr::null());
+        gl.CompileShader(fs);
+
+        let program = gl.CreateProgram();
+        gl.AttachShader(program, vs);
+        gl.AttachShader(program, fs);
+        gl.LinkProgram(program);
+        gl.UseProgram(program);
+
+        let mut vb = std::mem::zeroed();
+        gl.GenBuffers(1, &mut vb);
+        gl.BindBuffer(gl::ARRAY_BUFFER, vb);
+        gl.BufferData(
+            gl::ARRAY_BUFFER,
+            (VERTEX_DATA.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
+            VERTEX_DATA.as_ptr() as *const _,
+            gl::STATIC_DRAW,
+        );
+
+        if gl.BindVertexArray.is_loaded() {
+            let mut vao = std::mem::zeroed();
+            gl.GenVertexArrays(1, &mut vao);
+            gl.BindVertexArray(vao);
+        }
+
+        let pos_attrib = gl.GetAttribLocation(program, b"position\0".as_ptr() as *const _);
+        let color_attrib = gl.GetAttribLocation(program, b"color\0".as_ptr() as *const _);
+        gl.VertexAttribPointer(
+            pos_attrib as gl::types::GLuint,
+            2,
+            gl::FLOAT,
+            0,
+            5 * std::mem::size_of::<f32>() as gl::types::GLsizei,
+            std::ptr::null(),
+        );
+        gl.VertexAttribPointer(
+            color_attrib as gl::types::GLuint,
+            3,
+            gl::FLOAT,
+            0,
+            5 * std::mem::size_of::<f32>() as gl::types::GLsizei,
+            (2 * std::mem::size_of::<f32>()) as *const () as *const _,
+        );
+        gl.EnableVertexAttribArray(pos_attrib as gl::types::GLuint);
+        gl.EnableVertexAttribArray(color_attrib as gl::types::GLuint);
+    }
+
+    Gl { gl }
+}
+
+impl Gl {
+    pub fn draw_frame(&self, color: [f32; 4]) {
+        unsafe {
+            self.gl.ClearColor(color[0], color[1], color[2], color[3]);
+            self.gl.Clear(gl::COLOR_BUFFER_BIT);
+            self.gl.DrawArrays(gl::TRIANGLES, 0, 3);
+        }
+    }
+}
+
+#[rustfmt::skip]
+static VERTEX_DATA: [f32; 15] = [
+    -0.5, -0.5,  1.0,  0.0,  0.0,
+     0.0,  0.5,  0.0,  1.0,  0.0,
+     0.5, -0.5,  0.0,  0.0,  1.0,
+];
+
+const VS_SRC: &'static [u8] = b"
+#version 100
+precision mediump float;
+attribute vec2 position;
+attribute vec3 color;
+varying vec3 v_color;
+void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+    v_color = color;
+}
+\0";
+
+const FS_SRC: &'static [u8] = b"
+#version 100
+precision mediump float;
+varying vec3 v_color;
+void main() {
+    gl_FragColor = vec4(v_color, 1.0);
+}
+\0";
+
+pub use self::context_tracker::{ContextCurrentWrapper, ContextId, ContextTracker, ContextWrapper};
+
+#[allow(dead_code)] // Not used by all examples
+mod context_tracker {
+    use glutin::{
+        self, Context, ContextCurrentState, ContextError, NotCurrent, PossiblyCurrent,
+        WindowedContext,
+    };
+    use takeable_option::Takeable;
+
+    pub enum ContextWrapper<T: ContextCurrentState> {
+        Headless(Context<T>),
+        Windowed(WindowedContext<T>),
+    }
+
+    impl<T: ContextCurrentState> ContextWrapper<T> {
+        pub fn headless(&mut self) -> &mut Context<T> {
+            match self {
+                ContextWrapper::Headless(ref mut ctx) => ctx,
+                _ => panic!(),
+            }
+        }
+
+        pub fn windowed(&mut self) -> &mut WindowedContext<T> {
+            match self {
+                ContextWrapper::Windowed(ref mut ctx) => ctx,
+                _ => panic!(),
+            }
+        }
+
+        fn map<T2: ContextCurrentState, FH, FW>(
+            self,
+            fh: FH,
+            fw: FW,
+        ) -> Result<ContextWrapper<T2>, (Self, ContextError)>
+        where
+            FH: FnOnce(Context<T>) -> Result<Context<T2>, (Context<T>, ContextError)>,
+            FW: FnOnce(
+                WindowedContext<T>,
+            )
+                -> Result<WindowedContext<T2>, (WindowedContext<T>, ContextError)>,
+        {
+            match self {
+                ContextWrapper::Headless(ctx) => match fh(ctx) {
+                    Ok(ctx) => Ok(ContextWrapper::Headless(ctx)),
+                    Err((ctx, err)) => Err((ContextWrapper::Headless(ctx), err)),
+                },
+                ContextWrapper::Windowed(ctx) => match fw(ctx) {
+                    Ok(ctx) => Ok(ContextWrapper::Windowed(ctx)),
+                    Err((ctx, err)) => Err((ContextWrapper::Windowed(ctx), err)),
+                },
+            }
+        }
+    }
+
+    pub enum ContextCurrentWrapper {
+        PossiblyCurrent(ContextWrapper<PossiblyCurrent>),
+        NotCurrent(ContextWrapper<NotCurrent>),
+    }
+
+    impl ContextCurrentWrapper {
+        fn map_possibly<F>(self, f: F) -> Result<Self, (Self, ContextError)>
+        where
+            F: FnOnce(
+                ContextWrapper<PossiblyCurrent>,
+            ) -> Result<
+                ContextWrapper<NotCurrent>,
+                (ContextWrapper<PossiblyCurrent>, ContextError),
+            >,
+        {
+            match self {
+                ret @ ContextCurrentWrapper::NotCurrent(_) => Ok(ret),
+                ContextCurrentWrapper::PossiblyCurrent(ctx) => match f(ctx) {
+                    Ok(ctx) => Ok(ContextCurrentWrapper::NotCurrent(ctx)),
+                    Err((ctx, err)) => Err((ContextCurrentWrapper::PossiblyCurrent(ctx), err)),
+                },
+            }
+        }
+
+        fn map_not<F>(self, f: F) -> Result<Self, (Self, ContextError)>
+        where
+            F: FnOnce(
+                ContextWrapper<NotCurrent>,
+            ) -> Result<
+                ContextWrapper<PossiblyCurrent>,
+                (ContextWrapper<NotCurrent>, ContextError),
+            >,
+        {
+            match self {
+                ret @ ContextCurrentWrapper::PossiblyCurrent(_) => Ok(ret),
+                ContextCurrentWrapper::NotCurrent(ctx) => match f(ctx) {
+                    Ok(ctx) => Ok(ContextCurrentWrapper::PossiblyCurrent(ctx)),
+                    Err((ctx, err)) => Err((ContextCurrentWrapper::NotCurrent(ctx), err)),
+                },
+            }
+        }
+    }
+
+    pub type ContextId = usize;
+    #[derive(Default)]
+    pub struct ContextTracker {
+        current: Option<ContextId>,
+        others: Vec<(ContextId, Takeable<ContextCurrentWrapper>)>,
+        next_id: ContextId,
+    }
+
+    impl ContextTracker {
+        pub fn insert(&mut self, ctx: ContextCurrentWrapper) -> ContextId {
+            let id = self.next_id;
+            self.next_id += 1;
+
+            if let ContextCurrentWrapper::PossiblyCurrent(_) = ctx {
+                if let Some(old_current) = self.current {
+                    unsafe {
+                        self.modify(old_current, |ctx| {
+                            ctx.map_possibly(|ctx| {
+                                ctx.map(
+                                    |ctx| Ok(ctx.treat_as_not_current()),
+                                    |ctx| Ok(ctx.treat_as_not_current()),
+                                )
+                            })
+                        })
+                        .unwrap()
+                    }
+                }
+                self.current = Some(id);
+            }
+
+            self.others.push((id, Takeable::new(ctx)));
+            id
+        }
+
+        pub fn remove(&mut self, id: ContextId) -> ContextCurrentWrapper {
+            if Some(id) == self.current {
+                self.current.take();
+            }
+
+            let this_index = self.others.binary_search_by(|(sid, _)| sid.cmp(&id)).unwrap();
+            Takeable::take(&mut self.others.remove(this_index).1)
+        }
+
+        fn modify<F>(&mut self, id: ContextId, f: F) -> Result<(), ContextError>
+        where
+            F: FnOnce(
+                ContextCurrentWrapper,
+            )
+                -> Result<ContextCurrentWrapper, (ContextCurrentWrapper, ContextError)>,
+        {
+            let this_index = self.others.binary_search_by(|(sid, _)| sid.cmp(&id)).unwrap();
+
+            let this_context = Takeable::take(&mut self.others[this_index].1);
+
+            match f(this_context) {
+                Err((ctx, err)) => {
+                    self.others[this_index].1 = Takeable::new(ctx);
+                    Err(err)
+                }
+                Ok(ctx) => {
+                    self.others[this_index].1 = Takeable::new(ctx);
+                    Ok(())
+                }
+            }
+        }
+
+        pub fn get_current(
+            &mut self,
+            id: ContextId,
+        ) -> Result<&mut ContextWrapper<PossiblyCurrent>, ContextError> {
+            unsafe {
+                let this_index = self.others.binary_search_by(|(sid, _)| sid.cmp(&id)).unwrap();
+                if Some(id) != self.current {
+                    let old_current = self.current.take();
+
+                    if let Err(err) = self.modify(id, |ctx| {
+                        ctx.map_not(|ctx| {
+                            ctx.map(|ctx| ctx.make_current(), |ctx| ctx.make_current())
+                        })
+                    }) {
+                        // Oh noes, something went wrong
+                        // Let's at least make sure that no context is current.
+                        if let Some(old_current) = old_current {
+                            if let Err(err2) = self.modify(old_current, |ctx| {
+                                ctx.map_possibly(|ctx| {
+                                    ctx.map(
+                                        |ctx| ctx.make_not_current(),
+                                        |ctx| ctx.make_not_current(),
+                                    )
+                                })
+                            }) {
+                                panic!(
+                                    "Could not `make_current` nor `make_not_current`, {:?}, {:?}",
+                                    err, err2
+                                );
+                            }
+                        }
+
+                        if let Err(err2) = self.modify(id, |ctx| {
+                            ctx.map_possibly(|ctx| {
+                                ctx.map(|ctx| ctx.make_not_current(), |ctx| ctx.make_not_current())
+                            })
+                        }) {
+                            panic!(
+                                "Could not `make_current` nor `make_not_current`, {:?}, {:?}",
+                                err, err2
+                            );
+                        }
+
+                        return Err(err);
+                    }
+
+                    self.current = Some(id);
+
+                    if let Some(old_current) = old_current {
+                        self.modify(old_current, |ctx| {
+                            ctx.map_possibly(|ctx| {
+                                ctx.map(
+                                    |ctx| Ok(ctx.treat_as_not_current()),
+                                    |ctx| Ok(ctx.treat_as_not_current()),
+                                )
+                            })
+                        })
+                        .unwrap();
+                    }
+                }
+
+                match *self.others[this_index].1 {
+                    ContextCurrentWrapper::PossiblyCurrent(ref mut ctx) => Ok(ctx),
+                    ContextCurrentWrapper::NotCurrent(_) => panic!(),
+                }
+            }
+        }
+    }
+}
+
 
